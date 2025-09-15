039647b07786d0cdabe524732c0ec26ea0914c27
Nithish D <146643466+Nithishdj@users.noreply.github.com>
2025-09-15 18:28:24 +0530

    Add files via upload

---
+++ DefaultConstructor.java
@@ -0,0 +1,13 @@
+package com.index;
+//Default constructor
+public class DefaultConstructor {
+	DefaultConstructor()//default constructor because no args
+	{
+		System.out.print("This is my default constructor");
+		}
+	public static void main(String[] args) {
+		//constructor is invoked while creating an object of MyClass
+		DefaultConstructor obj = new DefaultConstructor();
+	}
+}
+

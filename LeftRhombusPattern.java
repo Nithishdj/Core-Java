fbc40cabf7c4221c9eb2283b610f1d6516ef7797
Nithish D <146643466+Nithishdj@users.noreply.github.com>
2025-10-10 12:25:27 +0530

    Add files via upload

---
+++ LeftRhombusPattern.java
@@ -0,0 +1,24 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class LeftRhombusPattern {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.print("Enter the number: ");
+		int n = scn.nextInt();
+		
+		int i,j,s;
+		for(i=0;i<n;i++) {
+			for(s=0;s<i;s++) {
+				System.out.print("  ");
+			}
+			for(j=0;j<n+1;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		scn.close();
+	}
+}
